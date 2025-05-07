a531d1c782ff0163abe16357ec0979c7fc960afe
KAYakovlev <the.dark.strike.tm@gmail.com>
2025-05-07 09:43:17 +0300

    Закончил верстать Projects

--- src/components/Menu/Projects.tsx
+++ src/components/Menu/Projects.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import plus_icon from "../../../src/assets/images/Add_Plus.png"
+import delete_icon from "../../../src/assets/images/Trash_Full.png"
+import paperclip from "../../../src/assets/images/Paperclip_Attechment_Tilt.png"
 import {useCreateProjectMutation, useGetAllProjectsQuery} from "../../api/testApi.ts";
 import {useNavigate} from "react-router-dom";
 
@@ -75,10 +77,20 @@ export const Projects = () => {
                                 onClick={() => {
                                     navigate(`/workspace/${project.id}`)
                                 }}
-                                className="w-[calc(50%-12.5px)] bg-[#171C20] rounded-[10px] px-[25px] cursor-pointer border-[#515558] border-[1px] h-[86px]">
-                                <div className="flex flex-col p-[21px]">
-                                    <span className="text-[#FFF] font-[Inter] font-regular text-[20px]">{project.title}</span>
-                                    <div className="text-[#FFF] font-[Inter] font-regular text-[12px]">Last edit - </div>
+                                className="w-[calc(50%-12.5px)] bg-[#171C20] rounded-[10px] p-[25px] cursor-pointer border-[#515558] border-[1px] ">
+                                <div className="flex flex-row justify-between items-center">
+                                    <div className="flex flex-col text-left gap-[5px]">
+                                        <span className="text-[#FFF] font-[Inter] font-regular text-[20px]">{project.title}</span>
+                                        <div className="text-[#FFF] font-[Inter] font-regular text-[12px]">Last updated - 27/04/2024</div>
+                                    </div>
+                                    <div className="flex flex-row items-center h-full gap-[24px]">
+                                        <button className="flex bg-[#171C20]  border-0 p-0 focus:outline-none cursor-pointer items-center justify-center">
+                                            <img className="w-[24px] h-[24px]" src={paperclip} alt=""/>
+                                        </button>
+                                        <button className="flex bg-[#171C20] border-0 p-0 focus:outline-none cursor-pointer items-center justify-center">
+                                            <img className="w-[24px] h-[24px]" src={delete_icon} alt=""/>
+                                        </button>
+                                    </div>
                                 </div>
                             </button>
                         ))}
@@ -89,20 +101,31 @@ export const Projects = () => {
                 <div className="flex flex-col w-full gap-[16px] mt-[56px] ">
                     <div className="text-[#FFF] font-[Inter] font-semibold text-[40px]">Latest Projects</div>
                     <div className="text-[#FFF] font-[Inter] font-semibold text-[20px]">12/07/24</div>
-                    <div className="flex flex-row mt-[30px] gap-[25px] flex-wrap">
+                    <div className="flex flex-row w-full mt-[30px] gap-[25px] flex-wrap">
                         {projectsData?.projects.map((project) => (
                             <button
                                 key={project.id}
                                 onClick={() => {
-                                    navigate(`/workspace/${project.id}`);
+                                    navigate(`/workspace/${project.id}`)
                                 }}
-                                className="w-[calc(50%-12.5px)] bg-[#171C20] rounded-[10px] px-[25px] cursor-pointer border-[#515558] border-[1px] h-[86px]">
-                                <div className="flex flex-col p-[21px]">
-                                    <span className="text-[#FFF] font-[Inter] font-regular text-[20px]">{project.title}</span>
-                                    <div className="text-[#FFF] font-[Inter] font-regular text-[12px]">Last edit - </div>
+                                className="w-[calc(50%-12.5px)] bg-[#171C20] rounded-[10px] p-[25px] cursor-pointer border-[#515558] border-[1px] ">
+                                <div className="flex flex-row justify-between items-center">
+                                    <div className="flex flex-col text-left gap-[5px]">
+                                        <span className="text-[#FFF] font-[Inter] font-regular text-[20px]">{project.title}</span>
+                                        <div className="text-[#FFF] font-[Inter] font-regular text-[12px]">Last updated - 27/04/2024</div>
+                                    </div>
+                                    <div className="flex flex-row items-center h-full gap-[24px]">
+                                        <button className="flex bg-[#171C20]  border-0 p-0 focus:outline-none cursor-pointer items-center justify-center">
+                                            <img className="w-[24px] h-[24px]" src={paperclip} alt=""/>
+                                        </button>
+                                        <button className="flex bg-[#171C20] border-0 p-0 focus:outline-none cursor-pointer items-center justify-center">
+                                            <img className="w-[24px] h-[24px]" src={delete_icon} alt=""/>
+                                        </button>
+                                    </div>
                                 </div>
                             </button>
                         ))}
+                        <div className="w-full h-[10%]"></div>
                     </div>
                 </div>
             </div>
