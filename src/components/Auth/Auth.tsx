fd12e725d1da299cd71049331b76ccf9580b13c5
KAYakovlev <the.dark.strike.tm@gmail.com>
2025-05-10 21:43:52 +0300

    test(will be rollback)

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,7 +1,7 @@
 import {useNavigate} from "react-router-dom";
 import {useLoginRequestMutation, useRegisterRequestMutation} from "../../api/testApi.ts";
-import React, {FormEvent, useState} from "react"; // Добавлен импорт useState и FormEvent
-import mail from "../../../src/assets/images/Mail.png"
+import React, {FormEvent, useState} from "react";
+
 import {motion} from "framer-motion";
 
 export const Auth = () => {
@@ -84,6 +84,7 @@ export const Auth = () => {
 
     };
 
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault();
         onAuth(formValues);
@@ -104,146 +105,7 @@ export const Auth = () => {
                 </div>
             )}
             */}
-                <form
-                    hidden={authFuncType == 'register'}
-                    onSubmit={handleSubmit}>
-                    <div className="flex flex-col border-[1px] border-[#515865] border-b-0 p-[19px] rounded-[15px] rounded-b-none bg-[rgba(17,25,40,0.5)] backdrop-blur-md">
-                        <div className="text-[#FFF] font-[Inter-semibold] text-[24px]">Login</div>
-                    </div>
-                    <div
-                        className="flex flex-col gap-[20px] border-[1px] border-[#515865] rounded-[15px] rounded-t-none bg-[rgba(17,25,40,0.5)] backdrop-blur-md"
-                    >
-                        <div className="flex flex-col py-[43px] px-[43px] gap-[17px]">
-                            <div className="flex flex-col">
-                                <div className="text-[#FFF] font-[Inter-semibold] text-[16px] py-[14px]">Email</div>
-                                <div className="flex flex-row">
-                                    <div className="p-[12px] border-[#575F69] rounded-[10px] rounded-r-none border-r-0 border-[1px] bg-[#1F2A37] flex items-center justify-center cursor-pointer">
-                                        <img src={mail} alt=""/>
-                                    </div>
-                                    <input
-                                        name="email"
-                                        type="text"
-                                        value={formValues.email}
-                                        onChange={handleInputChange}
-                                        placeholder="Placeholder"
-                                        className="flex w-[422px] h-[40px] px-[12px] py-[8px] border-[1px] rounded-l-none  border-[#575F69] rounded-[10px] bg-[#1F2A37] text-[#FFF] font-[Inter-medium] text-[20px] focus:outline-none">
 
-                                    </input>
-                                </div>
-                            </div>
-                            <div className="flex flex-col">
-                                <div className="text-[#FFF] font-[Inter-semibold] text-[16px] py-[14px]">Password</div>
-                                <div className="flex flex-row">
-                                    <div className="p-[12px] border-[#575F69] rounded-[10px] rounded-r-none border-r-0 border-[1px] bg-[#1F2A37] flex items-center justify-center cursor-pointer">
-                                        <img src={mail} alt=""/>
-                                    </div>
-                                    <input
-                                        name="password"
-                                        type="text"
-                                        value={formValues.password}
-                                        onChange={handleInputChange}
-                                        placeholder="Placeholder"
-                                        className="flex w-[422px] h-[40px] px-[12px] py-[8px] border-[1px] rounded-l-none  border-[#575F69] rounded-[10px] bg-[#1F2A37] text-[#FFF] font-[Inter-medium] text-[20px] focus:outline-none">
-
-                                    </input>
-                                </div>
-                            </div>
-                            <div className="pl-[11px] py-[13px] flex flex-row items-center gap-[13px]">
-                                <input type="checkbox" className="color-[#1F2A37] rounded-[5px] h-[25px] w-[25px]"/>
-                                <div className="text-[#FFF] font-[Inter-semibold] text-[20px]">Stay logged in</div>
-                            </div>
-                            <div className="py-[16.5px] flex flex-row justify-between">
-                                <div className="flex flex-col items-start">
-                                    <div className="text-[#FFF] font-[Inter-semibold] text-[20px]">Doesn’t have account?</div>
-                                    <a onClick={()=>{
-                                        setAuthFuncType("register");
-                                    }} className="text-[#6BFFA4] font-[Inter-semibold] text-[20px] cursor-pointer">Register</a>
-                                </div>
-                                <button className="bg-gradient-to-b from-[#4CAF72] to-[#3E945F] text-[#FFF] font-[Inter-medium] text-[16px] px-[35.5px] py-[15.5px] rounded-[10px] outline-none border-[1px] border-[#76BF92] cursor-pointer">
-                                    Login
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-                <form
-                    hidden={authFuncType == 'login'}
-                    onSubmit={handleSubmit}>
-                    <div className="flex flex-col border-[1px] border-[#515865] border-b-0 p-[19px] rounded-[15px] rounded-b-none bg-[rgba(17,25,40,0.5)] backdrop-blur-md">
-                        <div className="text-[#FFF] font-[Inter-semibold] text-[24px]">Register</div>
-                    </div>
-                    <div
-                        className="flex flex-col gap-[20px] border-[1px] border-[#515865] rounded-[15px] rounded-t-none bg-[rgba(17,25,40,0.5)] backdrop-blur-md"
-                    >
-                        <div className="flex flex-col py-[43px] px-[43px] gap-[17px]">
-                            <div className="flex flex-col">
-                                <div className="text-[#FFF] font-[Inter-semibold] text-[16px] py-[14px]">Email</div>
-                                <div className="flex flex-row">
-                                    <div className="p-[12px] border-[#575F69] rounded-[10px] rounded-r-none border-r-0 border-[1px] bg-[#1F2A37] flex items-center justify-center cursor-pointer">
-                                        <img src={mail} alt=""/>
-                                    </div>
-                                    <input
-                                        name="email"
-                                        type="text"
-                                        value={formValues.email}
-                                        onChange={handleInputChange}
-                                        placeholder="Placeholder" className="flex w-[422px] h-[40px] px-[12px] py-[8px] border-[1px] rounded-l-none  border-[#575F69] rounded-[10px] bg-[#1F2A37] text-[#FFF] font-[Inter-medium] text-[20px] focus:outline-none">
-
-                                    </input>
-                                </div>
-                            </div>
-                            <div className="flex flex-col">
-                                <div className="text-[#FFF] font-[Inter-semibold] text-[16px] py-[14px]">Password</div>
-                                <div className="flex flex-row">
-                                    <div className="p-[12px] border-[#575F69] rounded-[10px] rounded-r-none border-r-0 border-[1px] bg-[#1F2A37] flex items-center justify-center cursor-pointer">
-                                        <img src={mail} alt=""/>
-                                    </div>
-                                    <input
-                                        name="password"
-                                        type="text"
-                                        value={formValues.password}
-                                        onChange={handleInputChange}
-                                        placeholder="Placeholder"
-                                        className="flex w-[422px] h-[40px] px-[12px] py-[8px] border-[1px] rounded-l-none  border-[#575F69] rounded-[10px] bg-[#1F2A37] text-[#FFF] font-[Inter-medium] text-[20px] focus:outline-none">
-
-                                    </input>
-                                </div>
-                            </div>
-                            <div className="flex flex-col">
-                                <div className="text-[#FFF] font-[Inter-semibold] text-[16px] py-[14px]">Confirm password</div>
-                                <div className="flex flex-row">
-                                    <div className="p-[12px] border-[#575F69] rounded-[10px] rounded-r-none border-r-0 border-[1px] bg-[#1F2A37] flex items-center justify-center cursor-pointer">
-                                        <img src={mail} alt=""/>
-                                    </div>
-                                    <input
-                                        name="password_confirmation"
-                                        type="text"
-                                        value={formValues.password_confirmation}
-                                        onChange={handleInputChange}
-                                        placeholder="Placeholder"
-                                        className="flex w-[422px] h-[40px] px-[12px] py-[8px] border-[1px] rounded-l-none  border-[#575F69] rounded-[10px] bg-[#1F2A37] text-[#FFF] font-[Inter-medium] text-[20px] focus:outline-none">
-
-                                    </input>
-                                </div>
-                            </div>
-                            <div className="pl-[11px] py-[13px] flex flex-row items-center gap-[13px]">
-                                <input type="checkbox" className="color-[#1F2A37] rounded-[5px] h-[25px] w-[25px]"/>
-                                <div className="text-[#FFF] font-[Inter-semibold] text-[20px]">Accept TOS</div>
-                            </div>
-                            <div className="py-[16.5px] flex flex-row justify-between">
-                                <div className="flex flex-col">
-                                    <div className="text-[#FFF] font-[Inter-semibold] text-[20px]">Already registered?</div>
-                                    <a onClick={()=>{
-                                        setAuthFuncType("login");
-                                    }} className="text-[#6BFFA4] font-[Inter-semibold] text-[20px] cursor-pointer">Login</a>
-                                </div>
-                                <button className="bg-gradient-to-b from-[#4CAF72] to-[#3E945F] text-[#FFF] font-[Inter-medium] text-[16px] px-[35.5px] py-[15.5px] rounded-[10px] outline-none border-[1px] border-[#76BF92] cursor-pointer">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </form>
             </div>
         </motion.div>
     );
