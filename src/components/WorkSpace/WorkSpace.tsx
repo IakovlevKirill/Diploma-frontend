4e9664f2013ea0f2123b5edcb57b2ebf7df23099
KAYakovlev <the.dark.strike.tm@gmail.com>
2025-05-10 20:46:06 +0300

    Начал делать ноду(какой то бредик с шрифтами)

--- src/components/WorkSpace/WorkSpace.tsx
+++ src/components/WorkSpace/WorkSpace.tsx
@@ -39,6 +39,66 @@ export const WorkSpace = () => {
         };
     }, [dispatch]); // Добавил dispatch в зависимости useEffect
 
+    interface NodeGateWayProps {
+        type: string;
+    }
+
+    interface GateWayDotProps {
+        type: string;
+    }
+
+    const GatewayDot = (props: GateWayDotProps) => {
+        return (
+            <>
+                {(props.type == 'input') && (
+                    <div className="cursor-pointer hover:border-[1px] hover:border-[#FFF] border-[#7CD4ED] border-[1px] left-[-5px] absolute w-[10px] h-[10px] bg-[#7CD4ED] rounded-[100%]"></div>
+                )}
+                {(props.type == 'output') && (
+                    <div className="cursor-pointer hover:border-[1px] hover:border-[#FFF] border-[#7CD4ED] border-[1px] right-[-5px] absolute w-[10px] h-[10px] bg-[#7CD4ED] rounded-[100%]"></div>
+                )}
+            </>
+        )
+    }
+
+    const NodeGateway = (props: NodeGateWayProps) => {
+        return (
+            <>
+                {(props.type == "input") && (
+                    <div className="cursor-pointer flex flex-row items-center gap-[10px]">
+                        <GatewayDot type={"input"}></GatewayDot>
+                        <div className="font-[sans-serif] text-[14px]">Input 1</div>
+                    </div>
+                )}
+                {(props.type == "output") && (
+                    <div className="cursor-pointer flex flex-row items-center gap-[10px]">
+                        <div className="font-[sans-serif] text-[14px]">Output</div>
+                        <GatewayDot type={"output"}></GatewayDot>
+                    </div>
+                )}
+            </>
+        )
+    }
+
+    const Node = () => {
+        return(
+            <div className="relative w-[200px] flex flex-col justify-center rounded-[15px] rounded text-[#FFF] border-[1px] border-[#505356]">
+                <div className="flex justify-center w-[100%] font-[sans-serif] text-[14px] py-[7px] border-b-[1px] border-[#505356]">
+                    Math / Add
+                </div>
+                <div className="w-[calc(100%-30px)] flex flex-row items-center px-[15px] py-[20px] justify-center  text-[12px]">
+                    <div className="w-[50%] flex flex-col gap-[10px]">
+                        <NodeGateway type={'input'}></NodeGateway>
+                        <NodeGateway type={'input'}></NodeGateway>
+                        <NodeGateway type={'input'}></NodeGateway>
+                    </div>
+                    <div className="w-[50%] flex flex-col items-end justify-center gap-[10px]">
+                        <NodeGateway type={'output'}></NodeGateway>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div className="flex flex-col h-screen w-screen overflow-hidden">
             <LayoutBar></LayoutBar>
@@ -48,9 +108,7 @@ export const WorkSpace = () => {
                 transition={{ delay: 0.1 }}
                 className="h-[95%] w-[100vw] flex items-center justify-center"
             >
-                <div className="font-[Inter-semibold] text-[#FFF] ">
-                    WorkSpace пока что в разработке
-                </div>
+                <Node></Node>
             </motion.div>
 
         </div>
