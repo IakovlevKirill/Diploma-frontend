463a10fccafbd4ef0d3f3179566d276df85d5d5f
KAYakovlev <the.dark.strike.tm@gmail.com>
2025-05-12 21:20:11 +0300

    sidebar fix

--- src/components/Menu/MenuLayout.tsx
+++ src/components/Menu/MenuLayout.tsx
@@ -41,7 +41,7 @@ export const MenuLayout = () => {
                         </div>
                     </div>
                     <div className="w-full flex flex-col gap-[15px] border-t-[1px] border-[#535558] pt-[50px] ">
-                        <div className="flex w-[80%] animation_transform_sidebar_icon relative items-center flex-row justify-between ">
+                        <div className="flex w-[80%]  relative items-center flex-row justify-between ">
                             <button
                                 onClick={(e) => OnChangePage({page: "projects", e})}
                                 className={`
@@ -62,21 +62,21 @@ export const MenuLayout = () => {
 
                         <button
                             onClick={(e) => OnChangePage({page: "inbox", e})}
-                            className="animation_transform_sidebar_icon flex flex-row items-center gap-[15px] px-[30px] cursor-pointer bg-[#191C21] border-0">
+                            className=" flex flex-row items-center gap-[15px] px-[30px] cursor-pointer bg-[#191C21] border-0">
                             <img className="w-[24px] h-[24px]" src={images.inbox_icon_white} alt=""/>
                             <div className={`text-[#FFF] font-[Inter-medium]  text-[16px]
                                 ${currentPage == 'inbox' ? 'text-[24px]' : '' }`}>Inbox</div>
                         </button>
                         <button
                             onClick={(e) => OnChangePage({page: "teams", e})}
-                            className="animation_transform_sidebar_icon flex flex-row items-center gap-[15px] px-[30px] cursor-pointer bg-[#191C21] border-0">
+                            className=" flex flex-row items-center gap-[15px] px-[30px] cursor-pointer bg-[#191C21] border-0">
                             <img className="w-[24px] h-[24px]" src={images.team_icon_white} alt=""/>
                             <div className={`text-[#FFF] font-[Inter-medium]  text-[16px]
                                 ${currentPage == 'teams' ? 'text-[24px]' : '' }`}>Teams</div>
                         </button>
                         <button
                             onClick={(e) => OnChangePage({page: "templates", e})}
-                            className="animation_transform_sidebar_icon flex flex-row items-center gap-[15px] px-[30px] cursor-pointer bg-[#191C21] border-0">
+                            className=" flex flex-row items-center gap-[15px] px-[30px] cursor-pointer bg-[#191C21] border-0">
                             <img className="w-[24px] h-[24px]" src={images.templates_icon_white} alt=""/>
                             <div className={`text-[#FFF] font-[Inter-medium]  text-[16px]
                                 ${currentPage == 'templates' ? 'text-[24px]' : '' }`}>Templates</div>
