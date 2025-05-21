3a3daef55514c72a1d1cdbc4a43fc865111be34c
KAYakovlev <the.dark.strike.tm@gmail.com>
2025-05-22 01:05:05 +0300

    Переделал и улучшил компонент Project на странице меню

--- src/components/Menu/Projects/Projects.tsx
+++ src/components/Menu/Projects/Projects.tsx
@@ -20,8 +20,6 @@ export const Projects = () => {
     const { data: projectsData, isLoading: isAllProjectsLoading } = useGetAllProjectsQuery(current_user_userId);
     const { data: pinnedProjectsData, isLoading: isPinnedProjectsLoading } = useGetPinnedProjectQuery(current_user_userId);
 
-    console.log(pinnedProjectsData)
-
     const onCreateProject = async () => {
         if (current_user_userId) {
             try {
@@ -37,6 +35,61 @@ export const Projects = () => {
             }
         }
     };
+
+    interface ProjectComponentProps {
+        key: string;
+        id: string
+        title: string
+        updatedAt: string
+    }
+
+    const ProjectComponent = (props: ProjectComponentProps) => {
+        return (
+            <div
+                key={props.id}
+                className="w-[calc(50%-12.5px)] bg-[#171C20] rounded-[10px]  border-[#515558] border-[1px] box-border relative">
+                <div className="flex flex-row justify-between items-center">
+                    <button
+                        onClick={() => {
+                            navigate(`/workspace/${props.id}`);
+                        }}
+                        className="
+                                            z-1
+                                            py-[25px] pl-[25px]
+                                            w-[calc(100%)] h-[calc(100%)] flex flex-col text-left gap-[5px] bg-transparent cursor-pointer border-0">
+                        <span className="text-[#FFF] font-[Inter-normal] text-[20px]">{props.title}</span>
+                        <div className="text-[#FFF] font-[Inter-normal] text-[12px]">Last updated - {new Date(props.updatedAt).toLocaleDateString('en-GB')}</div>
+                    </button>
+
+                    <div className="z-2 absolute right-[25px]
+                                         flex flex-row items-start justify-end">
+                        <button
+                            onClick={() => {
+                                pinProject({projectId: props.id})
+                            }}
+                            className="flex items-center justify-center bg-transparent border-0 focus:outline-none cursor-pointer h-[36px] w-[36px] px-[0] py-[0]">
+                            <img className="w-[24px] h-[24px] p-[6px] hover:bg-[#FFFFFF13] hover:rounded-[5px]" src={images.Paperclip_Attechment_Tilt} alt=""/>
+                        </button>
+                        <button
+                            onClick={() => {
+                                navigate(`/workspace/${props.id}`);
+                            }}
+                            className="flex w-[14px] h-[36px] bg-[#171C20] border-[0px] cursor-pointer">
+
+                        </button>
+                        <button
+                            onClick={() => {
+                                const id = props.id
+                                deleteProject(id)
+                            }}
+                            className="flex items-center justify-center bg-transparent border-0 focus:outline-none cursor-pointer h-[36px] w-[36px] px-[0] py-[0]">
+                            <img className="w-[24px] h-[24px] p-[6px] hover:bg-[#FFFFFF13] hover:rounded-[5px]" src={images.Trash_Full} alt=""/>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 
     return (
         <motion.div
@@ -76,37 +129,12 @@ export const Projects = () => {
                         <div className="text-[#FFF] font-[Inter-semibold] text-[40px]">Pinned Projects</div>
                         <div className="flex flex-row w-full mt-[30px] flex-wrap" style={{ gap: "25px" }}>
                             {pinnedProjectsData?.projects.map((project) => (
-                                <div
+                                <ProjectComponent
                                     key={project.id}
-                                    className="w-[calc(50%-12.5px)] bg-[#171C20] rounded-[10px]  border-[#515558] border-[1px] box-border">
-                                    <div className="flex flex-row justify-between items-center">
-                                        <button
-                                            onClick={() => {
-                                                navigate(`/workspace/${project.id}`);
-                                            }}
-                                            className="p-[25px] pr-[0px] w-[70%] flex flex-col text-left gap-[5px] bg-transparent cursor-pointer border-0 p-0">
-                                            <span className="text-[#FFF] font-[Inter-normal] text-[20px]">{project.title}</span>
-                                            <div className="text-[#FFF] font-[Inter-normal] text-[12px]">Last updated - {new Date(project.updatedAt).toLocaleDateString('en-GB')}</div>
-                                        </button>
-                                        <div className="w-[30%] p-[25px] pl-[0px] flex flex-row items-start justify-end h-full gap-[14px]">
-                                            <button
-                                                onClick={() => {
-                                                    pinProject({projectId: project.id})
-                                                }}
-                                                className="flex bg-transparent border-0 focus:outline-none cursor-pointer">
-                                                <img className="w-[24px] h-[24px] p-[6px] hover:bg-[#FFFFFF13] hover:rounded-[5px]" src={images.Paperclip_Attechment_Tilt} alt=""/>
-                                            </button>
-                                            <button
-                                                onClick={() => {
-                                                    const id = project.id
-                                                    deleteProject(id)
-                                                }}
-                                                className="flex bg-transparent border-0 focus:outline-none cursor-pointer">
-                                                <img className="w-[24px] h-[24px] p-[6px] hover:bg-[#FFFFFF13] hover:rounded-[5px]" src={images.Trash_Full} alt=""/>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
+                                    id={project.id}
+                                    title={project.title}
+                                    updatedAt={project.updatedAt}
+                                ></ProjectComponent>
                             ))}
                         </div>
                         <div className="mt-[53px] w-full h-[1px] bg-[#4E5053]"></div>
@@ -116,37 +144,12 @@ export const Projects = () => {
                         <div className="text-[#FFF] font-[Inter-semibold] text-[20px]">12/07/24</div>
                         <div className="flex flex-row w-full mt-[30px] gap-[25px] flex-wrap">
                             {projectsData?.projects.map((project) => (
-                                <div
+                                <ProjectComponent
                                     key={project.id}
-                                    className="w-[calc(50%-12.5px)] bg-[#171C20] rounded-[10px]  border-[#515558] border-[1px] box-border">
-                                    <div className="flex flex-row justify-between items-center">
-                                        <button
-                                            onClick={() => {
-                                                navigate(`/workspace/${project.id}`);
-                                            }}
-                                            className="p-[25px] pr-[0px] w-[70%] flex flex-col text-left gap-[5px] bg-transparent cursor-pointer border-0 p-0">
-                                            <span className="text-[#FFF] font-[Inter-normal] text-[20px]">{project.title}</span>
-                                            <div className="text-[#FFF] font-[Inter-normal] text-[12px]">Last updated - {new Date(project.updatedAt).toLocaleDateString('en-GB')}</div>
-                                        </button>
-                                        <div className="w-[30%] p-[25px] pl-[0px] flex flex-row items-start justify-end h-full gap-[14px]">
-                                            <button
-                                                onClick={() => {
-                                                    pinProject({projectId: project.id})
-                                                }}
-                                                className="flex bg-transparent border-0 focus:outline-none cursor-pointer">
-                                                <img className="w-[24px] h-[24px] p-[6px] hover:bg-[#FFFFFF13] hover:rounded-[5px]" src={images.Paperclip_Attechment_Tilt} alt=""/>
-                                            </button>
-                                            <button
-                                                onClick={() => {
-                                                    const id = project.id
-                                                    deleteProject(id)
-                                                }}
-                                                className="flex bg-transparent border-0 focus:outline-none cursor-pointer">
-                                                <img className="w-[24px] h-[24px] p-[6px] hover:bg-[#FFFFFF13] hover:rounded-[5px]" src={images.Trash_Full} alt=""/>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
+                                    id={project.id}
+                                    title={project.title}
+                                    updatedAt={project.updatedAt}
+                                ></ProjectComponent>
                             ))}
                             <div className="w-full h-[10%]"></div>
                         </div>
