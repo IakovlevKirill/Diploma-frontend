e05a9d2c8e554edb28afbf054c22a2db6decdd44
KAYakovlev <the.dark.strike.tm@gmail.com>
2025-05-30 02:52:20 +0300

    Переверстал форму аутентификации

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -4,6 +4,7 @@ import React, {FormEvent, useState} from "react"; // Добавлен импор
 import {images} from '../../assets/images/images'
 import {motion} from "framer-motion";
 import {useDocumentTitle} from "../../app/hooks.ts";
+import {LayoutBar} from "../LayoutBar.tsx";
 
 export const Auth = () => {
 
@@ -99,28 +100,18 @@ export const Auth = () => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay: 0.1 }}
-            className=""
+            className="h-screen w-screen flex flex-col"
         >
-            <div className="h-screen w-screen relative items-center justify-center flex flex-col bg-gradient-to-t from-[#65CBE2] to-[#1D2237]">
-                {/*
-            {isRegisterLoading || isLoginLoading && (
-                <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-                    <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-white"></div>
-                </div>
-            )}
-            */}
-                <form
-                    hidden={authFuncType == 'register'}
-                    onSubmit={handleSubmit}>
-                    <div className="flex flex-col border-[1px] border-[#515865] border-b-0 p-[19px] rounded-[15px] rounded-b-none bg-[rgba(17,25,40,0.5)] backdrop-blur-md">
-                        <div className="text-[#FFF] font-[Inter-semibold] text-[24px]">Login</div>
+            <LayoutBar></LayoutBar>
+            <div className="h-[95%] relative items-center justify-center flex flex-col bg-gradient-to-t from-[#65CBE2] to-[#1D2237]">
+                <form hidden={!(authFuncType == 'login')} onSubmit={handleSubmit}>
+                    <div className="flex flex-col border-[1px] border-[#515865] border-b-0 p-[15px] rounded-[15px] rounded-b-none bg-[rgba(17,25,40,0.5)] backdrop-blur-md">
+                        <div className="text-[#FFF] font-[Inter-semibold] text-[20px]">Login</div>
                     </div>
-                    <div
-                        className="flex flex-col gap-[20px] border-[1px] border-[#515865] rounded-[15px] rounded-t-none bg-[rgba(17,25,40,0.5)] backdrop-blur-md"
-                    >
-                        <div className="flex flex-col py-[43px] px-[43px] gap-[17px]">
+                    <div className="flex flex-col gap-[20px] border-[1px] border-[#515865] rounded-[15px] rounded-t-none bg-[rgba(17,25,40,0.5)] backdrop-blur-md">
+                        <div className="flex flex-col p-[30px] gap-[17px]">
                             <div className="flex flex-col">
-                                <div className="text-[#FFF] font-[Inter-semibold] text-[16px] py-[14px]">Email</div>
+                                <div className="text-[#FFF] font-[Inter-semibold] text-[14px] py-[14px]">Email</div>
                                 <div className="flex flex-row">
                                     <div className="p-[12px] border-[#575F69] rounded-[10px] rounded-r-none border-r-0 border-[1px] bg-[#1F2A37] flex items-center justify-center cursor-pointer">
                                         <img src={images.mail} alt=""/>
@@ -131,13 +122,12 @@ export const Auth = () => {
                                         value={formValues.email}
                                         onChange={handleInputChange}
                                         placeholder="Placeholder"
-                                        className="flex w-[422px] h-[40px] px-[12px] py-[8px] border-[1px] rounded-l-none  border-[#575F69] rounded-[10px] bg-[#1F2A37] text-[#FFF] font-[Inter-medium] text-[20px] focus:outline-none">
-
+                                        className="flex px-[12px] py-[8px] border-[1px] rounded-l-none  border-[#575F69] rounded-[10px] bg-[#1F2A37] text-[#FFF] font-[Inter-medium] text-[16px] focus:outline-none">
                                     </input>
                                 </div>
                             </div>
                             <div className="flex flex-col">
-                                <div className="text-[#FFF] font-[Inter-semibold] text-[16px] py-[14px]">Password</div>
+                                <div className="text-[#FFF] font-[Inter-semibold] text-[14px] py-[14px]">Password</div>
                                 <div className="flex flex-row">
                                     <div className="p-[12px] border-[#575F69] rounded-[10px] rounded-r-none border-r-0 border-[1px] bg-[#1F2A37] flex items-center justify-center cursor-pointer">
                                         <img src={images.mail} alt=""/>
@@ -148,24 +138,19 @@ export const Auth = () => {
                                         value={formValues.password}
                                         onChange={handleInputChange}
                                         placeholder="Placeholder"
-                                        className="flex w-[422px] h-[40px] px-[12px] py-[8px] border-[1px] rounded-l-none  border-[#575F69] rounded-[10px] bg-[#1F2A37] text-[#FFF] font-[Inter-medium] text-[20px] focus:outline-none">
-
+                                        className="flex px-[12px] py-[8px] border-[1px] rounded-l-none  border-[#575F69] rounded-[10px] bg-[#1F2A37] text-[#FFF] font-[Inter-medium] text-[16px] focus:outline-none">
                                     </input>
                                 </div>
                             </div>
-                            <div className="pl-[11px] py-[13px] flex flex-row items-center gap-[13px]">
-                                <input type="checkbox" className="color-[#1F2A37] rounded-[5px] h-[25px] w-[25px]"/>
-                                <div className="text-[#FFF] font-[Inter-semibold] text-[20px]">Stay logged in</div>
-                            </div>
                             <div className="py-[16.5px] flex flex-row justify-between">
                                 <div className="flex flex-col items-start">
-                                    <div className="text-[#FFF] font-[Inter-semibold] text-[20px]">Doesn’t have account?</div>
+                                    <div className="text-[#FFF] font-[Inter-semibold] text-[16px]">Doesn’t have account?</div>
                                     <a onClick={()=>{
                                         setAuthFuncType("register");
-                                    }} className="text-[#6BFFA4] font-[Inter-semibold] text-[20px] cursor-pointer">Register</a>
+                                    }} className="text-[#6BFFA4] font-[Inter-semibold] text-[16px] cursor-pointer">Register</a>
                                 </div>
                                 <button
-                                    className={`bg-gradient-to-b from-[#4CAF72] to-[#3E945F] text-[#FFF] font-[Inter-medium] text-[16px] px-[35.5px] py-[15.5px] rounded-[10px] outline-none border-[1px] border-[#76BF92] cursor-pointer flex items-center justify-center ${
+                                    className={`bg-gradient-to-b from-[#4CAF72] to-[#3E945F] text-[#FFF] font-[Inter-medium] text-[14px] px-[25px] py-[10px] rounded-[10px] outline-none border-[1px] border-[#76BF92] cursor-pointer flex items-center justify-center ${
                                         isRegisterLoading || isLoginLoading ? 'opacity-75' : ''
                                     }`}
                                     disabled={isRegisterLoading || isLoginLoading}
@@ -186,18 +171,14 @@ export const Auth = () => {
                         </div>
                     </div>
                 </form>
-                <form
-                    hidden={authFuncType == 'login'}
-                    onSubmit={handleSubmit}>
-                    <div className="flex flex-col border-[1px] border-[#515865] border-b-0 p-[19px] rounded-[15px] rounded-b-none bg-[rgba(17,25,40,0.5)] backdrop-blur-md">
-                        <div className="text-[#FFF] font-[Inter-semibold] text-[24px]">Register</div>
+                <form hidden={!(authFuncType == 'register')} onSubmit={handleSubmit}>
+                    <div className="flex flex-col border-[1px] border-[#515865] border-b-0 p-[15px] rounded-[15px] rounded-b-none bg-[rgba(17,25,40,0.5)] backdrop-blur-md">
+                        <div className="text-[#FFF] font-[Inter-semibold] text-[20px]">Register</div>
                     </div>
-                    <div
-                        className="flex flex-col gap-[20px] border-[1px] border-[#515865] rounded-[15px] rounded-t-none bg-[rgba(17,25,40,0.5)] backdrop-blur-md"
-                    >
-                        <div className="flex flex-col py-[43px] px-[43px] gap-[17px]">
+                    <div className="flex flex-col gap-[20px] border-[1px] border-[#515865] rounded-[15px] rounded-t-none bg-[rgba(17,25,40,0.5)] backdrop-blur-md">
+                        <div className="flex flex-col p-[30px] gap-[17px]">
                             <div className="flex flex-col">
-                                <div className="text-[#FFF] font-[Inter-semibold] text-[16px] py-[14px]">Email</div>
+                                <div className="text-[#FFF] font-[Inter-semibold] text-[14px] py-[14px]">Email</div>
                                 <div className="flex flex-row">
                                     <div className="p-[12px] border-[#575F69] rounded-[10px] rounded-r-none border-r-0 border-[1px] bg-[#1F2A37] flex items-center justify-center cursor-pointer">
                                         <img src={images.mail} alt=""/>
@@ -207,13 +188,14 @@ export const Auth = () => {
                                         type="text"
                                         value={formValues.email}
                                         onChange={handleInputChange}
-                                        placeholder="Placeholder" className="flex w-[422px] h-[40px] px-[12px] py-[8px] border-[1px] rounded-l-none  border-[#575F69] rounded-[10px] bg-[#1F2A37] text-[#FFF] font-[Inter-medium] text-[20px] focus:outline-none">
+                                        placeholder="Placeholder"
+                                        className="flex px-[12px] py-[8px] border-[1px] rounded-l-none  border-[#575F69] rounded-[10px] bg-[#1F2A37] text-[#FFF] font-[Inter-medium] text-[16px] focus:outline-none">
 
                                     </input>
                                 </div>
                             </div>
                             <div className="flex flex-col">
-                                <div className="text-[#FFF] font-[Inter-semibold] text-[16px] py-[14px]">Password</div>
+                                <div className="text-[#FFF] font-[Inter-semibold] text-[14px] py-[14px]">Password</div>
                                 <div className="flex flex-row">
                                     <div className="p-[12px] border-[#575F69] rounded-[10px] rounded-r-none border-r-0 border-[1px] bg-[#1F2A37] flex items-center justify-center cursor-pointer">
                                         <img src={images.mail} alt=""/>
@@ -224,13 +206,12 @@ export const Auth = () => {
                                         value={formValues.password}
                                         onChange={handleInputChange}
                                         placeholder="Placeholder"
-                                        className="flex w-[422px] h-[40px] px-[12px] py-[8px] border-[1px] rounded-l-none  border-[#575F69] rounded-[10px] bg-[#1F2A37] text-[#FFF] font-[Inter-medium] text-[20px] focus:outline-none">
-
+                                        className="flex px-[12px] py-[8px] border-[1px] rounded-l-none  border-[#575F69] rounded-[10px] bg-[#1F2A37] text-[#FFF] font-[Inter-medium] text-[16px] focus:outline-none">
                                     </input>
                                 </div>
                             </div>
                             <div className="flex flex-col">
-                                <div className="text-[#FFF] font-[Inter-semibold] text-[16px] py-[14px]">Confirm password</div>
+                                <div className="text-[#FFF] font-[Inter-semibold] text-[14px] py-[14px]">Confirm password</div>
                                 <div className="flex flex-row">
                                     <div className="p-[12px] border-[#575F69] rounded-[10px] rounded-r-none border-r-0 border-[1px] bg-[#1F2A37] flex items-center justify-center cursor-pointer">
                                         <img src={images.mail} alt=""/>
@@ -241,24 +222,19 @@ export const Auth = () => {
                                         value={formValues.password_confirmation}
                                         onChange={handleInputChange}
                                         placeholder="Placeholder"
-                                        className="flex w-[422px] h-[40px] px-[12px] py-[8px] border-[1px] rounded-l-none  border-[#575F69] rounded-[10px] bg-[#1F2A37] text-[#FFF] font-[Inter-medium] text-[20px] focus:outline-none">
-
+                                        className="flex px-[12px] py-[8px] border-[1px] rounded-l-none  border-[#575F69] rounded-[10px] bg-[#1F2A37] text-[#FFF] font-[Inter-medium] text-[16px] focus:outline-none">
                                     </input>
                                 </div>
                             </div>
-                            <div className="pl-[11px] py-[13px] flex flex-row items-center gap-[13px]">
-                                <input type="checkbox" className="color-[#1F2A37] rounded-[5px] h-[25px] w-[25px]"/>
-                                <div className="text-[#FFF] font-[Inter-semibold] text-[20px]">Accept TOS</div>
-                            </div>
                             <div className="py-[16.5px] flex flex-row justify-between">
                                 <div className="flex flex-col">
-                                    <div className="text-[#FFF] font-[Inter-semibold] text-[20px]">Already registered?</div>
+                                    <div className="text-[#FFF] font-[Inter-semibold] text-[16px]">Already registered?</div>
                                     <a onClick={()=>{
                                         setAuthFuncType("login");
-                                    }} className="text-[#6BFFA4] font-[Inter-semibold] text-[20px] cursor-pointer">Login</a>
+                                    }} className="text-[#6BFFA4] font-[Inter-semibold] text-[16px] cursor-pointer">Login</a>
                                 </div>
                                 <button
-                                    className={`bg-gradient-to-b from-[#4CAF72] to-[#3E945F] text-[#FFF] font-[Inter-medium] text-[16px] px-[35.5px] py-[15.5px] rounded-[10px] outline-none border-[1px] border-[#76BF92] cursor-pointer flex items-center justify-center ${
+                                    className={`bg-gradient-to-b from-[#4CAF72] to-[#3E945F] text-[#FFF] font-[Inter-medium] text-[14px] px-[25px] py-[10px] rounded-[10px] outline-none border-[1px] border-[#76BF92] cursor-pointer flex items-center justify-center ${
                                         isRegisterLoading || isLoginLoading ? 'opacity-75' : ''
                                     }`}
                                     disabled={isRegisterLoading || isLoginLoading}
